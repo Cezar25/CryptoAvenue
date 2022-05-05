411066ba551e092548c72871560c6a1318064753
Cezar Buna <cezar.buna@amdaris.com>
2022-05-05 23:21:11 +0300

    added non-logged navbar and routing

--- CryptoAvenueFe/src/app/app.component.spec.ts
+++ CryptoAvenueFe/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CryptoAvenueFe'`, () => {
+  it(`should have as title 'CryptoAvenueFE'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CryptoAvenueFe');
+    expect(app.title).toEqual('CryptoAvenueFE');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('CryptoAvenueFe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CryptoAvenueFE app is running!');
   });
 });
--- CryptoAvenueFe/src/app/app.module.ts
+++ CryptoAvenueFe/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { MarketsComponent } from './components/markets/markets.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MarketsComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
